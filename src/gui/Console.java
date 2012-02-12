6dac3a0ee042407ea72198dc42d6381bd9ba8175
Zach Taylor <trees.wait@gmail.com>
2012-02-12 15:41:26 -0600

    Made console more "safe" by adding more console features.

--- src/gui/Console.java
+++ src/gui/Console.java
@@ -28,6 +28,16 @@ public class Console extends JTextField implements ActionListener {
         Pokemon target;
         int pos;
         boolean box = false;
+
+        // 1-argument flavor
+        if (string.size() == 1) {
+          Tools.notify("err", "All Pokemon", player.party.getNameList());
+          Driver.logConsoleEvent("Listing all pokemon\n"
+              + player.party.getNameList());
+          return;
+        }
+
+        // 2-argument flavor
         try {
           pos = Integer.parseInt(string.get(1));
         } catch (Exception e) {
@@ -45,8 +55,15 @@ public class Console extends JTextField implements ActionListener {
               + " doesn't exist : " + s);
           Driver.logConsoleEvent("Console event failure. Pokemon "
               + string.get(1) + " doesn't exist : " + s);
+          return;
+        }
+        if (string.size() == 2) {
+          Tools.notify(target, "Pokemon Lookup", target.toString());
+          Driver.logConsoleEvent("Looking up pokemon\n"+target.toString());
+          return;
         }
 
+        // x-argument flavor
         if (string.get(2).equals("heal")) {
           target.healDamage(target.health.max);
         }
@@ -58,10 +75,21 @@ public class Console extends JTextField implements ActionListener {
           target.resetTempStats();
           target.status.reset();
         }
+        else if (string.get(2).equals("move")) {
+          Tools.notify(target, "Pokemon Lookup", target.getMoveList());
+          Driver.logConsoleEvent("Looking up move list\n"+target.getMoveList());
+          return;
+        }
+        else if (string.get(2).equals("lead") && !box) {
+          Pokemon swap = player.party.pkmn[0];
+          player.party.pkmn[0] = target;
+          player.party.pkmn[pos] = swap;
+        }
         else {
           printError(s);
         }
       }
+
       else if (string.get(0).equals("bag")) {
         Item target;
         int pos;
@@ -85,6 +113,7 @@ public class Console extends JTextField implements ActionListener {
           printError(s);
         }
       }
+
       else if (string.get(0).equals("player")) {
         if (string.get(1).equals("cash")) {
           player.bag.cash += Integer.parseInt(string.get(2));
@@ -96,9 +125,11 @@ public class Console extends JTextField implements ActionListener {
           printError(s);
         }
       }
+
       else {
         printError(s);
       }
+
     } catch (Exception e) {
       printError(s);
     }
@@ -110,7 +141,8 @@ public class Console extends JTextField implements ActionListener {
   }
 
   private ArrayList<String> parse(String s) {
-    if (s == null) return null;
+    if (s == null)
+      return null;
 
     int length = s.length(), lastindex = 0;
 
