fd9817285af4b79cc86b155d63eca0bac8f3788e
Zach Taylor <zach.taylor673@gmail.com>
2012-06-22 17:02:04 -0500

    Formatting

--- src/jpkmn/game/pokemon/move/MoveStyle.java
+++ src/jpkmn/game/pokemon/move/MoveStyle.java
@@ -1,33 +1,57 @@
 package jpkmn.game.pokemon.move;
 
 public enum MoveStyle {
-    PHYSICAL, SPECIAL, OHKO, STATUS, REPEAT, DELAY, MISC;
+  PHYSICAL, SPECIAL, OHKO, STATUS, REPEAT, DELAY, MISC;
 
-    public int delay() {
-      return delay;
-    }
-    
-    public boolean attackAfterDelay() {
-      return attackAfterDelay;
-    }
-    
-    public boolean attackBeforeDelay() {
-      return !attackAfterDelay;
-    }
-    
-    public static MoveStyle valueOf(int style) {
-        switch (style) {
-            case 0:  return PHYSICAL;
-            case 1:  return SPECIAL;
-            case 2:  return OHKO;
-            case 3:  return STATUS;
-            case 4:  return REPEAT;
-            case 5:  return DELAY;
-            default: return MISC;
-        }
-    }
+  public int delay() {
+    return delay;
+  }
 
-    private int delay;
-    private boolean attackAfterDelay = true;
-    private static final int REPEAT_MIN = 2, REPEAT_EXTRA = 3;
+  public boolean attackAfterDelay() {
+    return attackAfterDelay;
+  }
+
+  public boolean attackBeforeDelay() {
+    return !attackAfterDelay;
+  }
+
+  public int getRepetitionAmount() {
+    if (this != REPEAT) return -1;
+
+    int amount = REPEAT_MIN;
+
+    if (Math.random() > .666) {
+      amount++;
+      if (Math.random() > .875) {
+        amount++;
+        if (Math.random() > .875)
+          amount++;
+      }
+    }
+    
+    return amount;
+  }
+
+  public static MoveStyle valueOf(int style) {
+    switch (style) {
+    case 0:
+      return PHYSICAL;
+    case 1:
+      return SPECIAL;
+    case 2:
+      return OHKO;
+    case 3:
+      return STATUS;
+    case 4:
+      return REPEAT;
+    case 5:
+      return DELAY;
+    default:
+      return MISC;
+    }
+  }
+
+  private int delay;
+  private boolean attackAfterDelay = true;
+  private static final int REPEAT_MIN = 2;
 }
