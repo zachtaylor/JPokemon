d0146e35dadaafffeb020976306f0872fec853c1
Zach Taylor <zach.taylor673@gmail.com>
2012-07-01 13:13:53 -0500

    MessageView explicit imports

--- src/jpkmn/exe/gui/MessageView.java
+++ src/jpkmn/exe/gui/MessageView.java
@@ -1,9 +1,20 @@
 package jpkmn.exe.gui;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.WindowConstants;
 
 import jpkmn.img.ImageFinder;
 
