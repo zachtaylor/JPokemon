1386b218f5954765818c67e5e7c974658d34449e
Zach Taylor <zach.taylor673@gmail.com>
2012-09-08 13:02:01 -0500

    EventType
    EventType privatized inside Event

--- src/jpkmn/map/Event.java
+++ src/jpkmn/map/Event.java
@@ -3,22 +3,26 @@ package jpkmn.map;
 import jpkmn.game.player.Player;
 
 public class Event {
-  public Event(Event.Type t, int int1, int int2) {
-    _type = t;
+  private enum Type {
+    GIFTPOKEMON, GIFTITEM, TRADEPOKEMON, TRADEITEM, BATTLE;
+
+    static Type valueOf(int t) {
+      return Type.values()[t];
+    }
+  }
+
+  public Event(int eventType, int int1, int int2) {
+    _type = Type.valueOf(eventType);
     _int1 = int1;
     _int2 = int2;
   }
 
-  public Event(Event.Type t, int int1) {
-    this(t, int1, -1);
+  public Event(int eventType, int int1) {
+    this(eventType, int1, -1);
   }
 
   public void trigger(Player p) {
     // TODO
-  }
-
-  public enum Type {
-    GIFTPOKEMON, GIFTITEM, TRADEPOKEMON, TRADEITEM, BATTLE;
   }
 
   private Event.Type _type;
