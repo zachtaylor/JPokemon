ee49b0660d9ab1425eeed99a983ca64221ad5607
Zach Taylor <zach.taylor673@gmail.com>
2013-05-26 13:56:12 -0500

    One RequirementSet per ActionSet
    Having multiple requirementsets/actionset was just hard and i don't want
    to do that anymore

--- src/org/jpokemon/action/RequirementSet.java
+++ src/org/jpokemon/action/RequirementSet.java
@@ -1,12 +1,13 @@
 package org.jpokemon.action;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.jpokemon.trainer.Player;
 import org.zachtaylor.jnodalxml.XMLNode;
 
-public class RequirementSet {
+public class RequirementSet implements Iterable<Requirement> {
   public static final String XML_NODE_NAME = "requirementset";
 
   public void add(Requirement r) {
@@ -45,5 +46,11 @@ public class RequirementSet {
     return this;
   }
 
+  @Override
+  public Iterator<Requirement> iterator() {
+    return _data.iterator();
+  }
+
   private List<Requirement> _data = new ArrayList<Requirement>();
+
 }
--- src/org/jpokemon/map/npc/NPCRequirement.java
+++ src/org/jpokemon/map/npc/NPCRequirement.java
@@ -8,7 +8,7 @@ import com.njkremer.Sqlite.DataConnectionManager;
 import com.njkremer.Sqlite.SqlStatement;
 
 public class NPCRequirement {
-  private int number, actionset, requirementset, type, data;
+  private int number, actionset, type, data;
 
   public static List<NPCRequirement> get(int number, int set) {
     DataConnectionManager.init(JPokemonConstants.DATABASE_PATH);
@@ -26,7 +26,6 @@ public class NPCRequirement {
   //@preformat
   public int getNumber() {return number;} public void setNumber(int n) {number = n;}
   public int getActionset() {return actionset;} public void setActionset(int s) {actionset = s;}
-  public int getRequirementset() {return requirementset;} public void setRequirementset(int r) {requirementset = r;}
   public int getType() {return type;} public void setType(int t) {type = t;}
   public int getData() {return data;} public void setData(int d) {data = d;}
   //@format
