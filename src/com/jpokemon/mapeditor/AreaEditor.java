686f427fb88d33e359d54213db2f95dc5453737f
Zach Taylor <zach.taylor673@gmail.com>
2013-05-26 01:35:01 -0500

    AreaEditor does Borders too #56
    AreaEditor will let you connect Areas with a Border

--- src/com/jpokemon/mapeditor/AreaEditor.java
+++ src/com/jpokemon/mapeditor/AreaEditor.java
@@ -4,19 +4,32 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.jpokemon.map.Area;
+import org.jpokemon.map.Border;
 
 import com.jpokemon.JPokemonButton;
 import com.jpokemon.mapeditor.widget.selector.AreaSelector;
+import com.njkremer.Sqlite.DataConnectionException;
+import com.njkremer.Sqlite.SqlStatement;
 
 public class AreaEditor implements MapEditComponent {
   public static final String BUTTON_NAME = "Areas";
 
   public AreaEditor() {
+    JButton newArea = new JPokemonButton("New");
+    newArea.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent arg0) {
+        AreaEditor.this.onClickNewArea();
+      }
+    });
+    editorPanel.add(newArea);
+
     areaSelector.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent arg0) {
         onClickSelectArea();
@@ -33,13 +46,16 @@ public class AreaEditor implements MapEditComponent {
     });
     editorPanel.add(nameField);
 
-    JButton newArea = new JPokemonButton("New");
-    newArea.addActionListener(new ActionListener() {
+    newBorderButton = new JButton("Add Border");
+    newBorderButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent arg0) {
-        AreaEditor.this.onClickNewArea();
+        onNewBorderButtonClick();
       }
     });
-    editorPanel.add(newArea);
+
+    childrenPanel.setLayout(new BoxLayout(childrenPanel, BoxLayout.Y_AXIS));
+    childrenPanel.add(newBorderButton);
+    editorPanel.add(childrenPanel);
   }
 
   @Override
@@ -50,6 +66,12 @@ public class AreaEditor implements MapEditComponent {
 
     if (areaSelector.getCurrentElement() != null) {
       nameField.setText(areaSelector.getCurrentElement().getName());
+
+      childrenPanel.removeAll();
+      for (Border border : Border.get(areaSelector.getCurrentElement().getNumber())) {
+        childrenPanel.add(new BorderPanel(border));
+      }
+      childrenPanel.add(newBorderButton);
     }
 
     readyToEdit = true;
@@ -58,7 +80,7 @@ public class AreaEditor implements MapEditComponent {
 
   @Override
   public Dimension getSize() {
-    return new Dimension(480, 40);
+    return new Dimension(480, 140);
   }
 
   private void onClickNewArea() {
@@ -85,12 +107,66 @@ public class AreaEditor implements MapEditComponent {
     getEditor();
   }
 
+  private void onNewBorderButtonClick() {
+    Border border = new Border();
+    border.setArea(areaSelector.getCurrentElement().getNumber());
+    border.setNext(0);
+
+    try {
+      SqlStatement.insert(border).execute();
+    } catch (DataConnectionException e) {
+      e.printStackTrace();
+    }
+
+    getEditor();
+  }
+
   private void commitChange() {
     areaSelector.getCurrentElement().commit();
   }
 
+  private class BorderPanel extends JPanel {
+    public BorderPanel(Border b) {
+      border = b;
+
+      add(new JLabel("can access: "));
+      add(new JPanel());
+
+      areaSelector.reload();
+      areaSelector.setSelectedIndex(border.getNext() - 1);
+      areaSelector.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent arg0) {
+          onAreaSelect();
+        }
+      });
+      add(areaSelector);
+    }
+
+    private void onAreaSelect() {
+      int oldNextArea = border.getNext();
+      int newNextArea = areaSelector.getCurrentElement().getNumber();
+
+      border.setNext(newNextArea);
+
+      try {
+        SqlStatement.update(border).where("area").eq(border.getArea()).and("next").eq(oldNextArea).execute();
+      } catch (DataConnectionException e) {
+        e.printStackTrace();
+      }
+
+      getEditor();
+    }
+
+    private Border border;
+    private AreaSelector areaSelector = new AreaSelector();
+
+    private static final long serialVersionUID = 1L;
+  }
+
+  private JButton newBorderButton;
   private boolean readyToEdit = false;
   private JPanel editorPanel = new JPanel();
+  private JPanel childrenPanel = new JPanel();
   private JTextField nameField = new JTextField();
   private AreaSelector areaSelector = new AreaSelector();
 }
